8cb0cc11801c16a8a69e307e65758041efe6d4c1
Israel Jasma <israel.jasma@gmail.com>
2023-05-24 22:26:57 -0400

    Feat: Proyecto Base Spring Boot

---
+++ src/main/java/cl/israeljasma/portafolios/PortafoliosApplication.java
@@ -0,0 +1,13 @@
+package cl.israeljasma.portafolios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafoliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafoliosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cl/israeljasma/portafolios/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cl.israeljasma.portafolios;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PortafoliosApplication.class);
+	}
+
+}
