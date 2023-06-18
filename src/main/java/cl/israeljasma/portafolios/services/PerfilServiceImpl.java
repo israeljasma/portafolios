cb3aa74ac9149c818c553338cb3c25ee803ee385
Israel Jasma <israel.jasma@gmail.com>
2023-06-18 14:02:24 -0400

    Feat: IPerfilService y PerfilServiceImpl

--- src/main/java/cl/israeljasma/portafolios/services/PerfilServiceImpl.java
+++ src/main/java/cl/israeljasma/portafolios/services/PerfilServiceImpl.java
@@ -1,2 +1,37 @@
-package cl.israeljasma.portafolios.services;public class PerfilServiceImpl {
+package cl.israeljasma.portafolios.services;
+
+import cl.israeljasma.portafolios.models.dao.IPerfilDao;
+import cl.israeljasma.portafolios.models.entity.Perfil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class PerfilServiceImpl implements IPerfilService {
+
+    @Autowired
+    private IPerfilDao perfilDao;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Perfil> findAll() {
+        return (List<Perfil>) perfilDao.findAll();
+    }
+
+    @Transactional
+    @Override
+    public void save(Perfil perfil) {
+        perfilDao.save(perfil);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Perfil finOne(Long id) {
+        return perfilDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(Long id) {
+        perfilDao.deleteById(id);
+    }
 }
