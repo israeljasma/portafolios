54abd1c4974e2813831026b02cebf18f279458ee
Israel Jasma <israel.jasma@gmail.com>
2023-06-18 13:35:09 -0400

    Update: cambios menores

--- src/main/java/cl/israeljasma/portafolios/models/services/IUsuarioService.java
+++ src/main/java/cl/israeljasma/portafolios/models/services/IUsuarioService.java
@@ -3,6 +3,7 @@ package cl.israeljasma.portafolios.models.services;
 import cl.israeljasma.portafolios.models.entity.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUsuarioService {
     public List<Usuario> findAll();
@@ -12,4 +13,5 @@ public interface IUsuarioService {
     public Usuario finOne(Long id);
 
     public void delete(Long id);
+    Optional<Usuario> findByUsername(String username);
 }
