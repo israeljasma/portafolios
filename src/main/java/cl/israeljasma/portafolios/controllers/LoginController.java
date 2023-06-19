fac86205e02e071694e7cacfd5ec9519ce12cd2d
Israel Jasma <israel.jasma@gmail.com>
2023-06-19 18:36:29 -0400

    Feat: comentarios loginController

--- src/main/java/cl/israeljasma/portafolios/controllers/LoginController.java
+++ src/main/java/cl/israeljasma/portafolios/controllers/LoginController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class LoginController {
 
+    /**
+     * Devuelve la vista de la página de inicio de sesión.
+     *
+     * @param model El modelo utilizado para pasar datos a la vista.
+     * @return La ruta de la vista de la página de inicio de sesión.
+     */
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage(Model model){
         model.addAttribute("titulo", "Listado de usuarios");
