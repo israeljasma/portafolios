959621b63fa78059fde7a1ca31521177194aa7c1
Israel Jasma <israel.jasma@gmail.com>
2023-06-19 19:28:27 -0400

    Feat: cometarios CreateLog

--- src/main/java/cl/israeljasma/portafolios/logs/CreateLog.java
+++ src/main/java/cl/israeljasma/portafolios/logs/CreateLog.java
@@ -8,11 +8,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class CreateLog {
+
+    /**
+     * Registra un mensaje de error en un archivo de registro.
+     *
+     * @param errorMensaje El mensaje de error a registrar.
+     */
     public static void errorLog(String errorMensaje) {
         doRutaMkdir(); // metodo que verifica ruta donde se creará log, sino existe crear la ruta con los directorios respectivos
         createFileLog(errorMensaje); // metodo que crea el archivo de log
     }
 
+
+    /**
+     * Crea un archivo de registro y escribe el mensaje de error.
+     *
+     * @param errorMensaje El mensaje de error a escribir en el archivo de registro.
+     */
     public static void createFileLog(String errorMensaje) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
         SimpleDateFormat dateFormatHora = new SimpleDateFormat("HH:mm:ss");
@@ -38,6 +50,9 @@ public class CreateLog {
         }
     }
 
+    /**
+     * Verifica la existencia de una ruta de directorio para el archivo de registro. Si no existe, crea la ruta y los directorios necesarios.
+     */
     public static void doRutaMkdir() {
         if (!(new File("/opt/tomcat/bin/proyectoWeb/Log/").exists())) {
             (new File("/opt/tomcat/bin/proyectoWeb/Log/")).mkdirs();
