02c6db060555eb3423f82fc5c7145e721b173f30
Israel Jasma <israel.jasma@gmail.com>
2023-06-21 17:38:13 -0400

    Fix: private faltante

--- src/main/java/cl/israeljasma/portafolios/services/ExperienciaLaboralServiceImpl.java
+++ src/main/java/cl/israeljasma/portafolios/services/ExperienciaLaboralServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class ExperienciaLaboralServiceImpl implements IExperienciaLaboralService {
 
     @Autowired
-    IExperienciaLaboralDao experienciaLaboralDao;
+    private IExperienciaLaboralDao experienciaLaboralDao;
 
     @Transactional(readOnly = true)
     @Override
