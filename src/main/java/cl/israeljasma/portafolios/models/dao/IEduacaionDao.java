fee909a680c8abbae8b90d3e0620e74b5923a95c
Israel Jasma <israel.jasma@gmail.com>
2023-06-21 17:04:39 -0400

    Feat: Dao educacion

---
+++ src/main/java/cl/israeljasma/portafolios/models/dao/IEduacaionDao.java
@@ -0,0 +1,7 @@
+package cl.israeljasma.portafolios.models.dao;
+
+import cl.israeljasma.portafolios.models.entity.Educacion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IEduacaionDao extends CrudRepository<Educacion, Long> {
+}
