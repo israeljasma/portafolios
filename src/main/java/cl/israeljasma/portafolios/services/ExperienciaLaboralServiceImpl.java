03ee05906178b1814491292627ad6c4aa2b2dc9c
Israel Jasma <israel.jasma@gmail.com>
2023-06-21 17:33:43 -0400

    Feat: Experiencia laboral service Implement

---
+++ src/main/java/cl/israeljasma/portafolios/services/ExperienciaLaboralServiceImpl.java
@@ -0,0 +1,37 @@
+package cl.israeljasma.portafolios.services;
+
+import cl.israeljasma.portafolios.models.dao.IExperienciaLaboralDao;
+import cl.israeljasma.portafolios.models.entity.ExperienciaLaboral;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class ExperienciaLaboralServiceImpl implements IExperienciaLaboralService {
+
+    @Autowired
+    IExperienciaLaboralDao experienciaLaboralDao;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<ExperienciaLaboral> findAll() {
+        return (List<ExperienciaLaboral>) experienciaLaboralDao.findAll();
+    }
+
+    @Transactional
+    @Override
+    public void save(ExperienciaLaboral experienciaLaboral) {
+        experienciaLaboralDao.save(experienciaLaboral);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public ExperienciaLaboral finOne(Long id) {
+        return experienciaLaboralDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(Long id) {
+        experienciaLaboralDao.deleteById(id);
+    }
+}
