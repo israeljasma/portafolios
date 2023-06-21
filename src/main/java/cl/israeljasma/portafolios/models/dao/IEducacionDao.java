0d79338a432a9438ee968317048c0da8dac28711
Israel Jasma <israel.jasma@gmail.com>
2023-06-21 17:42:28 -0400

    Fix: error en nombre

--- src/main/java/cl/israeljasma/portafolios/models/dao/IEducacionDao.java
+++ src/main/java/cl/israeljasma/portafolios/models/dao/IEducacionDao.java
@@ -3,5 +3,5 @@ package cl.israeljasma.portafolios.models.dao;
 import cl.israeljasma.portafolios.models.entity.Educacion;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IEduacaionDao extends CrudRepository<Educacion, Long> {
+public interface IEducacionDao extends CrudRepository<Educacion, Long> {
 }
